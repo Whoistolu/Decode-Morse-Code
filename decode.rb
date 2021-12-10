3e28a93e59f684f693c80a00c0a553385da36b04
Whoistolu <digittolu25@gmail.com>
2021-12-10 09:36:40 +0100

    Fix linter errors

--- decode.rb
+++ decode.rb
@@ -1,55 +1,54 @@
-MORSE_CODE = { 
-   '.-'=>     'a',
-   '-...'=>   'b',
-   '-.-.'=>   'c',
-   '-..'=>    'd',
-   '.'=>      'e',
-   '..-.'=>   'f',
-   '--.'=>    'g',
-   '....'=>   'h',
-   '..'=>     'i',
-   '.---'=>   'j',
-   '-.-'=>    'k',
-   '.-..'=>   'l',
-   '--'=>     'm',
-   '-.'=>     'n',
-   '---'=>    'o',
-   '.--.'=>   'p',
-   '--.-'=>   'q',
-   '.-.'=>    'r',
-   '...'=>    's',
-   '-'=>      't',
-   '..-'=>    'u',
-   '...-'=>   'v',
-   '.--'=>    'w',
-   '-..-'=>   'x',
-   '-.--'=>   'y',
-   '--..'=>   'z',
-}.freeze;
+MORSE_CODE = {
+  '.-' => 'a',
+  '-...' => 'b',
+  '-.-.' => 'c',
+  '-..' => 'd',
+  '.' => 'e',
+  '..-.' => 'f',
+  '--.' => 'g',
+  '....' => 'h',
+  '..' => 'i',
+  '.---' => 'j',
+  '-.-' => 'k',
+  '.-..' => 'l',
+  '--' => 'm',
+  '-.' => 'n',
+  '---' => 'o',
+  '.--.' => 'p',
+  '--.-' => 'q',
+  '.-.' => 'r',
+  '...' => 's',
+  '-' => 't',
+  '..-' => 'u',
+  '...-' => 'v',
+  '.--' => 'w',
+  '-..-' => 'x',
+  '-.--' => 'y',
+  '--..' => 'z'
+}.freeze
 
-def decode_word (code)
-    
-    result = ""
-    code = code.split(" ")
-    code.each { |key|
-       result += MORSE_CODE[key]
-    }
-    return result.upcase
+def decode_word(code)
+  result = ''
+  code = code.split(' ')
+  code.each do |key|
+    result += MORSE_CODE[key]
+  end
+  result.upcase
 end
-    
-def decode (str)
-   result = ''
-   str.strip.tr('/', " ").split('  ').each do |n|
-      arr = []
-      n.split.each do |n|
-         arr.push(MORSE_CODE[n])
-      end
-      result += "#{arr.join} "
-   end
-      return result.upcase
+
+def decode(str)
+  result = ''
+  str.strip.tr('/', ' ').split('  ').each do |n|
+    arr = []
+    n.split.each do |n|
+      arr.push(MORSE_CODE[n])
+    end
+    result += "#{arr.join} "
+  end
+  result.upcase
 end
 
 puts decode_word('.-')
 puts decode_word('-- -.--')
 puts decode('-- -.--   -. .- -- .')
-puts decode("      .-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-. / .-. ..- -... .. . ...")
+puts decode('      .-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-. / .-. ..- -... .. . ...')
